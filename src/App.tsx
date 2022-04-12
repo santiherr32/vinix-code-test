17873fc1e73026a793bdc2f903925a6f58f8e425
Santiago Herrera <santiherr32@gmail.com>
2022-04-11 20:58:51 -0500

    Changed some prettier configurations

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import Login from './Pages/Login';
 import Register from './Pages/Register';
 
 function App(): JSX.Element {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Home />}>
-                    <Route path="create" element={<Login />} />
-                </Route>
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="create" element={<Login />} />
+        </Route>
 
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </div>
-    );
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
