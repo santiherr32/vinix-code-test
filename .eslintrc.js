1b95bba7cb4217860cf9b6bdcb45894e0bec1fcc
Santiago Herrera <santiherr32@gmail.com>
2022-04-11 19:39:21 -0500

    Test 3 for new eslint configuration for nt workflow on Github

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,43 @@
 module.exports = {
     env: {
         browser: true,
+        node: true,
+        es6: true,
     },
+    plugins: [
+        '@typescript-eslint',
+    ],
     extends: ['airbnb-typescript', 'react-app', 'prettier'],
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    files: ['*.ts', '*.tsx'],
     parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        ecmaVersion: 'latest', // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
         ecmaFeatures: {
             jsx: true, // Allows for the parsing of JSX
         },
         project: './tsconfig.json',
     },
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            parserOptions: {
-                ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-                sourceType: 'module', // Allows for the use of imports
-                ecmaFeatures: {
-                    jsx: true, // Allows for the parsing of JSX
-                },
-                // this setting is required to use rules that require type information
-                project: './tsconfig.json',
-            },
-            rules: {
-                '@typescript-eslint/prefer-nullish-coalescing': 'error',
-            },
-        },
-    ],
+    rules: {
+        '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    },
+    // overrides: [
+    //     {
+    //         files: ['*.ts', '*.tsx'],
+    //         parserOptions: {
+    //             ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    //             sourceType: 'module', // Allows for the use of imports
+    //             ecmaFeatures: {
+    //                 jsx: true, // Allows for the parsing of JSX
+    //             },
+    //             // this setting is required to use rules that require type information
+    //             project: './tsconfig.json',
+    //         },
+    //         rules: {
+    //             '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    //         },
+    //     },
+    // ],
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
