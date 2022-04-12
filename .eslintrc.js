1ee49c1d97b096aafdbcc8a4c9a9e9539c735349
Santiago Herrera <santiherr32@gmail.com>
2022-04-11 19:21:48 -0500

    Test 2 for new eslint configuration for nt workflow on Github

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,11 @@ module.exports = {
     extends: ['airbnb-typescript', 'react-app', 'prettier'],
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
         project: './tsconfig.json',
     },
     overrides: [
