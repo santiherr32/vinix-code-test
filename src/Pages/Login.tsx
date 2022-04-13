ec63b6d26bdae25b8b9db3a41574a165f21afd6e
Santiago Herrera <santiherr32@gmail.com>
2022-04-13 09:03:19 -0500

    feature/login: Improved login form design and added some minimal verifications

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,4 +1,6 @@
 import { useState, useRef, ChangeEvent, FormEvent } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router';
 import './Login.scss';
 
 interface Data {
@@ -7,68 +9,100 @@ interface Data {
 }
 
 function Login() {
+  // const [tokenExists, setTokenExists] = useState(false);
+  // const { state, dispatch } = useContext(AppContext);
   const [inputs, setInputs] = useState<Data>({
     email: '',
     password: ''
   });
   const formRef = useRef(null);
+  const navigate = useNavigate();
 
-    const loginUser = async () => {
-        try {
-            const formData = new FormData(formRef.current);
-            const response = await (
-                await fetch(
-                    'http://front-test.vinixcode.cloud:8000/api/auth/login',
-                    {
-                        method: 'POST',
-                        body: formData,
-                    },
-                )
-            ).json();
-            // setInputs(data.inputs);
-            return response;
-        } catch (error) {
-            return error.message;
-        }
-    };
+  const loginUser = async () => {
+    try {
+      const formData = new FormData(formRef.current);
+      const response = await (
+        await fetch('http://front-test.vinixcode.cloud:8000/api/auth/login', {
+          method: 'POST',
+          body: formData
+        })
+      ).json();
+      // setInputs(data.inputs);
+      return response;
+    } catch (error) {
+      return error.message;
+    }
+  };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const inputName: string = e.target.name;
-        const inputValue: string = e.target.value;
-        e.preventDefault();
-        setInputs({
-            ...inputs,
-            [inputName]: inputValue,
-        });
-    };
-    const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        loginUser().then((data) => console.log(data.id));
-    };
-    return (
-        <form className="login-form" ref={formRef}>
-            <label htmlFor="email">Email: </label>
-            <input
-                type="email"
-                name="email"
-                id="email"
-                value={inputs.email}
-                onChange={handleChange}
-            />
-            <label htmlFor="password">Password: </label>
-            <input
-                type="password"
-                name="password"
-                id="password"
-                value={inputs.password}
-                onChange={handleChange}
-            />
-            <button type="submit" onClick={handleSubmit}>
-                Submit
-            </button>
-        </form>
-    );
+  const saveLocalLoginInfo = (info: {
+    access_token: string;
+    error: string;
+  }) => {
+    if (!info.access_token) {
+      if (info.error) alert('User was not found. Check again');
+      else alert(JSON.stringify(info));
+      return;
+    }
+    localStorage.setItem('token', info.access_token);
+    navigate('/');
+    // dispatch({
+    //     type: 'SET_TOKEN',
+    //     payload: info.access_token,
+    // })
+  };
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputName: string = e.target.name;
+    const inputValue: string = e.target.value;
+    e.preventDefault();
+    setInputs({
+      ...inputs,
+      [inputName]: inputValue
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (inputs.email && inputs.password) {
+      loginUser().then((data) => saveLocalLoginInfo(data));
+    }
+  };
+
+  return (
+    <form className="login-form" ref={formRef}>
+      <label className="form-label" htmlFor="email">
+        <span>Email: </span>
+        <input
+          className="form-control"
+          type="email"
+          name="email"
+          id="email"
+          value={inputs.email}
+          required={true}
+          onChange={handleChange}
+        />
+      </label>
+      <label className="form-label" htmlFor="password">
+        <span>Password: </span>
+        <input
+          className="form-control"
+          type="password"
+          name="password"
+          id="password"
+          required={true}
+          value={inputs.password}
+          onChange={handleChange}
+        />
+      </label>
+      <Button
+        type="submit"
+        onClick={handleSubmit}
+        disabled={inputs.email && inputs.password ? false : true}
+      >
+        Submit
+      </Button>
+    </form>
+  );
 }
 
 export default Login;
