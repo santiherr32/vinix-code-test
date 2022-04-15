92573e74c9f2231ea035d9c8e4cc4a9a5dd267eb
Santiago Herrera <santiherr32@gmail.com>
2022-04-14 21:58:55 -0500

    Minor change in app component's imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import './App.scss';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Create from './pages/Create';
-import { ContextProvider } from './Context';
+import Home from '../src/pages/Home';
+import Login from '../src/pages/Login';
+import Register from '../src/pages/Register';
+import Create from '../src/pages/Create';
+// import { ContextProvider } from './Context';
 
 function App(): JSX.Element {
   const token = localStorage.getItem('token');
@@ -19,17 +19,17 @@ function App(): JSX.Element {
 
   return (
     <div className="App">
-      <ContextProvider>
-        <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="/create" element={<Create />} />
-          <Route
-            path="/login"
-            element={tokenExists ? <Navigate to="/" /> : <Login />}
-          />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </ContextProvider>
+      {/* <ContextProvider> */}
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="/create" element={<Create />} />
+        <Route
+          path="/login"
+          element={tokenExists ? <Navigate to="/" /> : <Login />}
+        />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      {/* </ContextProvider> */}
     </div>
   );
 }
